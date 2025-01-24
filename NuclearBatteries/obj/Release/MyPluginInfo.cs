9ac15097bb4fd84989168756b935f283f6294699
M3dicCookie <pred102@hotmail.com>
2025-01-24 04:59:44 -0800

    Delete NuclearBatteries directory

--- NuclearBatteries/obj/Release/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace NuclearBatteries
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "NuclearBatteries";
-        public const string PLUGIN_NAME = "";
-        public const string PLUGIN_VERSION = "1.0.0.0";
-    }
-}
